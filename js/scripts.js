015b95bafc3cdad8e0b1ac6ac3a9375b6c5a10dd
akempffer <46541728+akempffer@users.noreply.github.com>
2019-05-20 00:06:35 -0700

    Added Java Script

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log('OK');
+const player = new Plyr( '#player' , {
+  
+    controls : ['play', 'progress', 'fullscreen', 'current-time', 'volume', 'settings'],
+    muted : false,
+   
+    
+    
+    
+  });
